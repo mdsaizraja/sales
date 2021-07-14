28e54ccbb52cba624369c70b7da174300d83387a
mdsaizraja <mdraja883@gmail.com>
2021-07-15 00:15:09 +0530

    debug f

--- Sales_project/settings.py
+++ Sales_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'treqktnk&-03$otc7%fxsw2a7ls%n&c2cq5#4p7n3o=ckvas@$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [ ]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
