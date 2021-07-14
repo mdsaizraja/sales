a38f866e318d6afe98763c724c9a3a4559b41b81
mdsaizraja <mdraja883@gmail.com>
2021-07-14 23:42:43 +0530

    whitenoise

--- Sales_project/settings.py
+++ Sales_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -139,4 +140,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 
 django_heroku.settings(locals())
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
