9d949ba427a8b001bce87e086206bf919aa6975d
mdsaizraja <mdraja883@gmail.com>
2021-07-14 13:44:55 +0530

    host

--- Sales_project/settings.py
+++ Sales_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'treqktnk&-03$otc7%fxsw2a7ls%n&c2cq5#4p7n3o=ckvas@$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1','salesdj.herokuapp.com']
 
 
 # Application definition
